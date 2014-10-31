a67bf0e7d5087c8db4da23b9c7ac7b16f1333179
nikesh <nikeshashar@gmail.com>
2014-10-31 18:15:11 +0000

    Hides no books notice when there is a book

--- spec/features/book_features_spec.rb
+++ spec/features/book_features_spec.rb
@@ -14,21 +14,28 @@ describe "Book Page" do
 
   it "has multiple books" do
     book = Book.create title: "Rails for Dummies"
-  	book2 = Book.create title: "Javascript for Dummies"
-  	visit books_path
-  	expect(page).to have_content "Rails for Dummies"
-  	expect(page).to have_content "Javascript for Dummies"
+    book2 = Book.create title: "Javascript for Dummies"
+    visit books_path
+    expect(page).to have_content "Rails for Dummies"
+    expect(page).to have_content "Javascript for Dummies"
   end
 
   it "can add a book to the shelf using a form" do
-  	visit books_path
-  	click_link 'Add a book'
-  	within '.book-form' do
-  		fill_in "Title", with: "Rails for Dummies"
-  		click_button "Add book"
-  	end
-  	expect(current_path).to eq books_path
-  	expect(page).to have_content "Rails for Dummies"
+    visit books_path
+    click_link 'Add a book'
+    within '.book-form' do
+      fill_in "Title", with: "Rails for Dummies"
+      click_button "Add book"
+    end
+    expect(current_path).to eq books_path
+    expect(page).to have_content "Rails for Dummies"
   end
+
+  it "hides no books notice when there is a book" do
+    book = Book.create title: "Rails for Dummies"
+    visit books_path
+    expect(page).not_to have_content "No books on this bookshelf"
+  end
+    
 
 end
